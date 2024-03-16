9bb22012315c9a8a3d45939899bf5f7833f00e27
kovacs7 <asishkumarbehera.official@gmail.com>
2024-03-16 11:35:38 +0530

    created a middleware and errorHandler for error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,13 +1,14 @@
 import User from "../models/user.model.js"
+import { errorHandler } from "../utils/error.js"
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body
     const newUser = new User({ username, email, password })
     try {
         await newUser.save()
         res.status(201).json({ "message": "user created successfully" })
     } catch (err) {
-        res.status(500).json(err.message)
+        next(err)
     }
 
 }
